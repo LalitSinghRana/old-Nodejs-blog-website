f8902ce45651c05eaf2b0da9d006e4a2d6ec7ffe
Lalit Rana <lalit.rana@nagarro.com>
2021-09-30 17:12:08 +0530

    just before mongoose

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,16 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('in articles');
+router.get('/new', (req, res) => {
+	try {
+		res.render('articles/new');
+	} catch (err) {
+		console.log(err);
+	}
 });
+
+// router.post('/', (req,res)=> {
+
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
 const app = express();
 
+// mongoose.connect('mongodb://localhost:5000/blog');
+// 								mongodmongodb://localhost:27017/{DATABASE_NAME}
 app.set('view engine', 'ejs');
 
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
-	res.render('index', { articles: articles });
+	const articles = [
+		{
+			title: 'Test Article 3',
+			createdAt: new Date(),
+			description: 'lorum ipsum',
+		},
+		{
+			title: 'Test Article 2',
+			createdAt: new Date(),
+			description: 'lorum ipsum 2',
+		},
+	];
+	res.render('articles/index', { articles: articles });
 });
 
 app.listen(5000);
